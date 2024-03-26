c875d3a0be73bd585cca196b23f1bb99fa7a09eb
Arian Salimy <asalimy@uci.edu>
2024-03-26 16:16:16 -0700

    more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-white">
+     <h1 className="text-3xl text-black">Give-It-Away</h1>
+
     </main>
   );
 }
