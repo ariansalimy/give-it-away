298f03164be4dd4f37afa475b64ff2d02c6af3e7
Arian Salimy <distortedflamingturtle@gmail.com>
2024-03-26 17:49:53 -0700

    dude i hate this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from "next/font/google" 
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 
+import './globals.css'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+       
+  
+        {children}
+
+        </AppRouterCacheProvider>
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Image from "next/image";
-
+import ButtonUsage from "./components/testbutton";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-white">
-     <h1 className="text-3xl text-black">Give-It-Away</h1>
+    <main className=" p-12">
+      <div className= "flex flex-row justify-center">
+     <h1 className="text-3xl font-bold text-black">Give-It-Away</h1>
+     </div>
+     <div className= "flex flex-row justify-center">
+    <h2 className = "text-2xl"> A giveaway randomizer tool.</h2>
+    </div>
+
+    <div className = "flex flex-row justify-center gap-2">
+      <div className = "flex flex-col">
+      1
+      </div>
+      <div className = "flex flex-col">
+        25
+      </div>
+    </div>
 
     </main>
   );
